98021f0a3487774e20457002384819c7829515cf
Color-Marker <AAAstudying0987@gmail.com>
2026-03-04 14:44:41 +0900

    feat: jpaRepository 형식으로 레포지토리 모두 전환 및 수정

--- discodeit/src/main/java/com/sprint/mission/discodeit/storage/BinaryContentStorage.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/storage/BinaryContentStorage.java
@@ -1,5 +1,15 @@
 package com.sprint.mission.discodeit.storage;
 
-public class BinaryContentStorage {
+import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
+import java.io.InputStream;
+import java.util.UUID;
+import org.springframework.http.ResponseEntity;
 
+public interface BinaryContentStorage {
+
+  UUID put(UUID binaryContentId, byte[] bytes);
+
+  InputStream get(UUID binaryContentId);
+
+  ResponseEntity<?> download(BinaryContentDto metaData);
 }
