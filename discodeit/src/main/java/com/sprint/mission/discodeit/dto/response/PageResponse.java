c8120af1d46e8cec9ad0c8fbba0c7c87a5c6b779
Color-Marker <AAAstudying0987@gmail.com>
2026-03-06 10:23:07 +0900

    feat: pageResponse dto와 mapper 추가

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/response/PageResponse.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/response/PageResponse.java
@@ -1,5 +1,13 @@
 package com.sprint.mission.discodeit.dto.response;
 
-public record PageResponse() {
+import java.util.List;
+
+public record PageResponse<T>(
+    List<T> content,
+    int number,
+    int size,
+    boolean hasNext,
+    Long totalElements
+) {
 
 }
