310f1a0b2539169bc944b2e1449ac7412a4eb8ce
Color-Marker <AAAstudying0987@gmail.com>
2026-03-27 09:26:03 +0900

    feat: 이메일 사이즈 설정 추가

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/UserCreateRequest.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/UserCreateRequest.java
@@ -9,12 +9,11 @@ import jakarta.validation.constraints.Size;
 public record UserCreateRequest(
     @NotBlank(message = "이름은 필수입니다.")
     @Size(min = 2, max = 20, message = "이름은 2자 이상 20자 이하로 입력해주세요")
-    @Pattern(regexp = "^[가-힣a-zA-Z\\s]+$",
-        message = "이름은 한글, 영문, 공백만 사용할 수 있습니다")
     @Schema(description = "이름", example = "guest")
     String username,
     @NotBlank(message = "이메일은 필수입니다.")
     @Email(message = "올바르 이메일 형식이 아닙니다.")
+    @Size(max = 100, message = "이메일은 100자를 초과할 수 없습니다")
     @Schema(description = "이메일", example = "guest@codeit.com")
     String email,
     @NotBlank(message = "비밀번호는 필수입니다.")
--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
@@ -7,10 +7,9 @@ import jakarta.validation.constraints.Size;
 
 public record UserUpdateRequest(
     @Size(min = 2, max = 20, message = "이름은 2자 이상 20자 이하로 입력해주세요")
-    @Pattern(regexp = "^[가-힣a-zA-Z\\s]+$",
-        message = "이름은 한글, 영문, 공백만 사용할 수 있습니다")
     @Schema(description = "새로운 이름", example = "guest")
     String newUsername,
+    @Size(max = 100, message = "이메일은 100자를 초과할 수 없습니다")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     @Schema(description = "새로운 이메일", example = "guest@codeit.com")
     String newEmail,
