feaff54c906c22e568239ec999c21cf43e8a5012
Color-Marker <AAAstudying0987@gmail.com>
2026-05-13 17:50:04 +0900

    feat: 로그인-로그아웃-회원가입 정상 작동 확인

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.sprint.mission.discodeit.entity.DiscodeitUserDetails;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -32,6 +33,9 @@ public class AuthController {
   public ResponseEntity<UserDto> getMe(
       @AuthenticationPrincipal DiscodeitUserDetails userDetails
   ) {
+    if (userDetails == null) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
     return ResponseEntity.ok(userDetails.getUserDto());
   }
 }
