e7e669ee910dd2375c7a49b64beb3da959a44ac9
Color-Marker <AAAstudying0987@gmail.com>
2026-05-18 14:55:50 +0900

    feat: 세션 관리 고도화 완료

--- discodeit/src/main/java/com/sprint/mission/discodeit/config/AdminInitializer.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/config/AdminInitializer.java
@@ -2,9 +2,7 @@ package com.sprint.mission.discodeit.config;
 
 import com.sprint.mission.discodeit.entity.Role;
 import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.repository.UserRepository;
-import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
@@ -17,7 +15,6 @@ import org.springframework.stereotype.Component;
 public class AdminInitializer implements ApplicationRunner {
 
   private final UserRepository userRepository;
-  private final UserStatusRepository userStatusRepository;
   private final PasswordEncoder passwordEncoder;
 
   @Override
@@ -33,9 +30,6 @@ public class AdminInitializer implements ApplicationRunner {
         null,
         Role.ADMIN
     );
-    Instant now = Instant.now();
-    UserStatus userStatus = new UserStatus(admin, now);
     userRepository.save(admin);
-    userStatusRepository.save(userStatus);
   }
 }
--- discodeit/src/main/java/com/sprint/mission/discodeit/service/basic/DiscodeitUserDetailsService.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/service/basic/DiscodeitUserDetailsService.java
@@ -5,6 +5,7 @@ import com.sprint.mission.discodeit.entity.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.mapper.UserMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
