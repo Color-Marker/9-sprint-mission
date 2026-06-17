844b1cd2eb374be693ac005fb54ab17458541836
Color-Marker <AAAstudying0987@gmail.com>
2026-06-17 13:27:42 +0900

    feat: 웹소켓 인증/인가 처리 완료

--- discodeit/src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -1,8 +1,20 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.entity.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,6 +23,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  private final JwtAuthenticationChannelInterceptor jwtAuthenticationChannelInterceptor;
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -26,5 +40,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   }
 
   // 웹소켓 인증/인가 처리
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+    registration.interceptors(
+        jwtAuthenticationChannelInterceptor,
+        new SecurityContextChannelInterceptor(),
+        authorizationChannelInterceptor());
+  }
+
+  private AuthorizationChannelInterceptor authorizationChannelInterceptor() {
+    return new AuthorizationChannelInterceptor(
+        MessageMatcherDelegatingAuthorizationManager.builder()
+            .simpTypeMatchers(SimpMessageType.CONNECT, SimpMessageType.DISCONNECT,
+                SimpMessageType.UNSUBSCRIBE).permitAll()
+            .simpTypeMatchers(SimpMessageType.SUBSCRIBE, SimpMessageType.MESSAGE).authenticated()
+            .anyMessage().hasRole(Role.USER.name())
+            .build()
+    );
+  }
 
 }
